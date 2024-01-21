cc18c1ea4c61c85fc9cc7096fd406e0e65b16abc
adarsh <adarshmaurya9415@gmail.com>
2024-01-21 23:22:30 +0530

    successfully working on init

--- api/mlApi/bert_darkp.py
+++ api/mlApi/bert_darkp.py
@@ -1,14 +1,251 @@
-from transformers import  TFAutoModel, AutoTokenizer
+# Implementing TowardsDS's BERT model for dark pattern detection
 
-# loading model
-model = TFAutoModel.from_pretrained("bert-base-uncased")
+import torch
+from tqdm import tqdm
 
-#loading tokenizer
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+from transformers import BertTokenizer
+from torch.utils.data import TensorDataset
 
-inputs = tokenizer("Hello, my dog is cute", return_tensors="tf", padding=True, truncation=True)
+from transformers import BertForSequenceClassification
 
-print(inputs)
+# Preparing and labeling DataSets
 
-output = model(input)
-print(output)
+import pandas as pd
+import numpy as np
+
+file_path = "F:/backup-kali/codeFiles/projects/cognigaurd/api/datasets/dataset.tsv"
+df = pd.read_csv(file_path ,sep="\t")
+
+tags_df = df['Category'].value_counts()
+
+# print(tags_df)
+
+# encoding the labels
+possible_labels = df.Category.unique()
+
+label_dict = {}
+for index, possible_label in enumerate(possible_labels):
+    label_dict[possible_label] = index
+
+# print(label_dict)
+df['label'] = df.Category.replace(label_dict)
+
+## Train and Validation split
+from sklearn.model_selection import train_test_split
+
+X_train, X_val, y_train, y_val = train_test_split(df.index.values, 
+                                                  df.label.values, 
+                                                  test_size=0.15, 
+                                                  random_state=42, 
+                                                  stratify=df.label.values)
+
+df['data_type'] = ['not_set']*df.shape[0]
+
+df.loc[X_train, 'data_type'] = 'train'
+df.loc[X_val, 'data_type'] = 'val'
+
+df.groupby(['Category', 'label', 'data_type']).count()
+
+# Bert Tokenizer and Encoding the data 
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', 
+                                          do_lower_case=True)
+                                          
+encoded_data_train = tokenizer.batch_encode_plus(
+    df[df.data_type=='train'].Title.values, 
+    add_special_tokens=True, 
+    return_attention_mask=True, 
+    pad_to_max_length=True, 
+    max_length=256, 
+    return_tensors='pt'
+)
+
+encoded_data_val = tokenizer.batch_encode_plus(
+    df[df.data_type=='val'].Title.values, 
+    add_special_tokens=True, 
+    return_attention_mask=True, 
+    pad_to_max_length=True, 
+    max_length=256, 
+    return_tensors='pt'
+)
+
+
+input_ids_train = encoded_data_train['input_ids']
+attention_masks_train = encoded_data_train['attention_mask']
+labels_train = torch.tensor(df[df.data_type=='train'].label.values)
+
+input_ids_val = encoded_data_val['input_ids']
+attention_masks_val = encoded_data_val['attention_mask']
+labels_val = torch.tensor(df[df.data_type=='val'].label.values)
+
+dataset_train = TensorDataset(input_ids_train, attention_masks_train, labels_train)
+dataset_val = TensorDataset(input_ids_val, attention_masks_val, labels_val)
+
+
+## Bert Pretrained Model
+
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased",
+                                                      num_labels=len(label_dict),
+                                                      output_attentions=False,
+                                                      output_hidden_states=False)
+
+## Data Loaders 
+
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+
+batch_size = 3
+
+dataloader_train = DataLoader(dataset_train, 
+                              sampler=RandomSampler(dataset_train), 
+                              batch_size=batch_size)
+
+dataloader_validation = DataLoader(dataset_val, 
+                                   sampler=SequentialSampler(dataset_val), 
+                                   batch_size=batch_size)
+
+
+
+## Optimizer Scheduler 
+
+from transformers import AdamW, get_linear_schedule_with_warmup
+
+optimizer = AdamW(model.parameters(),
+                  lr=1e-5, 
+                  eps=1e-8)
+                  
+epochs = 5
+
+scheduler = get_linear_schedule_with_warmup(optimizer, 
+                                            num_warmup_steps=0,
+                                            num_training_steps=len(dataloader_train)*epochs)
+
+
+## Performance Metrics Calculation
+
+from sklearn.metrics import f1_score
+
+def f1_score_func(preds, labels):
+    preds_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return f1_score(labels_flat, preds_flat, average='weighted')
+
+def accuracy_per_class(preds, labels):
+    label_dict_inverse = {v: k for k, v in label_dict.items()}
+    
+    preds_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+
+    for label in np.unique(labels_flat):
+        y_preds = preds_flat[labels_flat==label]
+        y_true = labels_flat[labels_flat==label]
+        print(f'Class: {label_dict_inverse[label]}')
+        print(f'Accuracy: {len(y_preds[y_preds==label])}/{len(y_true)}\n')
+
+
+## TraningLoop for BERT
+
+# for loading cpu model on gpu   
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+import random
+
+seed_val = 17
+random.seed(seed_val)
+np.random.seed(seed_val)
+torch.manual_seed(seed_val)
+torch.cuda.manual_seed_all(seed_val)
+
+def evaluate(dataloader_val):
+
+    model.eval()
+    
+    loss_val_total = 0
+    predictions, true_vals = [], []
+    
+    for batch in dataloader_val:
+        
+        batch = tuple(b.to(device) for b in batch)
+        
+        inputs = {'input_ids':      batch[0],
+                  'attention_mask': batch[1],
+                  'labels':         batch[2],
+                 }
+
+        with torch.no_grad():        
+            outputs = model(**inputs)
+            
+        loss = outputs[0]
+        logits = outputs[1]
+        loss_val_total += loss.item()
+
+        logits = logits.detach().cpu().numpy()
+        label_ids = inputs['labels'].cpu().numpy()
+        predictions.append(logits)
+        true_vals.append(label_ids)
+    
+    loss_val_avg = loss_val_total/len(dataloader_val) 
+    
+    predictions = np.concatenate(predictions, axis=0)
+    true_vals = np.concatenate(true_vals, axis=0)
+            
+    return loss_val_avg, predictions, true_vals
+    
+for epoch in tqdm(range(1, epochs+1)):
+    
+    model.train()
+    
+    loss_train_total = 0
+
+    progress_bar = tqdm(dataloader_train, desc='Epoch {:1d}'.format(epoch), leave=False, disable=False)
+    for batch in progress_bar:
+
+        model.zero_grad()
+        
+        batch = tuple(b.to(device) for b in batch)
+        
+        inputs = {'input_ids':      batch[0],
+                  'attention_mask': batch[1],
+                  'labels':         batch[2],
+                 }       
+
+        outputs = model(**inputs)
+        
+        loss = outputs[0]
+        loss_train_total += loss.item()
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        optimizer.step()
+        scheduler.step()
+        
+        progress_bar.set_postfix({'training_loss': '{:.3f}'.format(loss.item()/len(batch))})
+         
+        
+    torch.save(model.state_dict(), f'fine_tuned_bert/finetuned_BERT_epoch_{epoch}.model')
+        
+    tqdm.write(f'\nEpoch {epoch}')
+    
+    loss_train_avg = loss_train_total/len(dataloader_train)            
+    tqdm.write(f'Training loss: {loss_train_avg}')
+    
+    val_loss, predictions, true_vals = evaluate(dataloader_validation)
+    val_f1 = f1_score_func(predictions, true_vals)
+    tqdm.write(f'Validation loss: {val_loss}')
+    tqdm.write(f'F1 Score (Weighted): {val_f1}')
+
+
+# Loading and Evaluating Fine-Tuned BERT model
+    
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased",
+                                                      num_labels=len(label_dict),
+                                                      output_attentions=False,
+                                                      output_hidden_states=False)
+
+model.to(device)
+
+model.load_state_dict(torch.load('fine_tuned_bert/finetuned_BERT_epoch_1.model', map_location=torch.device('cpu')))
+
+_, predictions, true_vals = evaluate(dataloader_validation)
+accuracy_per_class(predictions, true_vals)
