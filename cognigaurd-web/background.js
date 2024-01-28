b8eb282f5e319c689fea7d462a9724689958b31c
Anmol Goel <mrgoel2975@gmail.com>
2024-01-28 18:04:03 +0530

    initial testing for popups done

--- cognigaurd-web/background.js
+++ cognigaurd-web/background.js
@@ -1,30 +1,44 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
-  if (changeInfo.status == 'complete') {
-    console.log("hello")
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo,tab) {
+  if (changeInfo.status == 'complete' && !tab.url.startsWith('chrome://')) {
     chrome.scripting.executeScript({
       target: { tabId: tabId },
-      files: ['popup_detect.js']
+      files: ['popup_detectV2.js']
     });
   }
 });
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+let lastMessageTime = Date.now();
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {      
       if (request.message === "open_popup") {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
           chrome.tabs.sendMessage(tabs[0].id, {"message": "open_popup"});
         });
       }
-      if (message.type == 'popupDetected') {
-        const tabId = sender.tab.id;
-        if (popup_cnt[tabId]) {
-          popup_cnt[tabId]++;
-        } else {
-          popup_cnt[tabId] = 1;
-        }
-        console.log('Popup detected on ' + sender.tab.url);
-        // console.log('Popup count for tab ' + tabId + ': ' + popup_cnt[tabId]);
+
+      const currentTime = Date.now();
+      
+      
+      if (currentTime - lastMessageTime < 3000) {
+        lastMessageTime = currentTime;
+        return; // Ignore the message if it's within 1 second of the previous message
       }
-    });
+
+      lastMessageTime = currentTime;
+
+      if (request.message == "center popup" || request.message == "side popup") {
+        const tabId = sender.tab.id;
+        
+        chrome.tabs.captureVisibleTab(null, {}, function (dataUrl){
+          console.log('Popup detected on ' + sender.tab.url);
+          //pass dataurl to backend with request.message
+        });
+        
+        // console.log('Popup count for tab ' + tabId + ': ' + popup_cnt[tabId]);
+      } else {
+        console.log(request.message);
+      }
+});
+
 
   
