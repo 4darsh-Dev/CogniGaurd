33590eba2b1e9a3888806d2901bd5c018fcf6b37
adarsh <adarshmaurya9415@gmail.com>
2024-02-12 10:25:54 +0530

    app for custom admin panel

---
+++ api/customadmin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CustomadminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'customadmin'
---
+++ api/customadmin/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+
+# Create your views here.
+
+def admin_login(request):
+    pass
