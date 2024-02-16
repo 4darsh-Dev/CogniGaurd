14f90eb41daa36e65a1ba62d126919f954fc96bf
adarsh <adarshmaurya9415@gmail.com>
2024-02-16 20:41:00 +0530

    updated for supervising datasets

--- api/mlApi/download.py
+++ api/mlApi/download.py
@@ -2,7 +2,7 @@
 
 # nltk.download('cmudict')
 import pandas as pd
-file_path = "F:/backup-kali/codeFiles/projects/cognigaurd/api/datasets/dp_dataset.tsv"
+file_path = "F:/backup-kali/codeFiles/projects/cognigaurd/api/datasets/new_dp_dataset.tsv"
 
 file_path2 = "F:/backup-kali/codeFiles/projects/cognigaurd/api/datasets/new_dp_dataset_final.tsv"
 
@@ -16,7 +16,7 @@ df2  = pd.read_csv(file_path2, sep='\t')
 # # Check for NaN values in the entire DataFrame
 # nan_values = df.isna().sum()
 
-dark_pattern_counts = df['Pattern Category'].value_counts()
+dark_pattern_counts = df['Category'].value_counts()
 
 dark_pattern_counts2 = df2['Category'].value_counts()   
 
@@ -36,3 +36,20 @@ print(dark_pattern_counts2)
 
 # new_df = df[['text', 'Pattern Category']]
 # new_df.to_csv('F:/backup-kali/codeFiles/projects/cognigaurd/api/datasets/new_dp_dataset.csv',sep="\t", index=False)
+
+
+import pandas as pd
+
+output_file_path = "F:/backup-kali/codeFiles/projects/cognigaurd/api/datasets/new_dp_dataset.csv"
+
+# Read the TSV file
+# df = pd.read_csv(file_path2, sep='\t')
+
+# # Write the DataFrame to a CSV file
+# df.to_csv(output_file_path, index=False)
+
+# print("TSV file converted to CSV successfully!")
+
+df3 = pd.read_csv(output_file_path)
+dark_pattern_counts3 = df3['Category'].value_counts()
+print(dark_pattern_counts3)
