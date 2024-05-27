11c9226a9bf1b28c460f567b17e6997ec710f031
ADARSH MAURYA <109789509+4darsh-Dev@users.noreply.github.com>
2024-05-28 00:55:44 +0530

    added url conf for deply

--- django-backend/cogniguard/urls.py
+++ django-backend/cogniguard/urls.py
@@ -29,3 +29,6 @@ urlpatterns = [
     path("", include("home.urls")),
     path("api/", include("mlApi.urls")),
 ]
+# vercel deployment url configuration
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
