83b24dadcfdc4e70e939737d69cbdf19f4994776
ADARSH MAURYA <109789509+4darsh-Dev@users.noreply.github.com>
2024-05-28 00:50:51 +0530

    updated for vercel deply

--- django-backend/cogniguard/wsgi.py
+++ django-backend/cogniguard/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cogniguard.settings')
 
 application = get_wsgi_application()
+# for vercel deployment setup
+app = application
