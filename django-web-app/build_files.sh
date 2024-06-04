3bf159beb4ad31e0067ea2518117d666f1b68682
ADARSH MAURYA <109789509+4darsh-Dev@users.noreply.github.com>
2024-06-04 13:03:26 +0530

    removed env creation

--- django-web-app/build_files.sh
+++ django-web-app/build_files.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Install pip and virtualenv if necessary
-python3 -m venv venv
-source venv/bin/activate
-
+# make migrations
+python manage.py migrate
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python manage.py collectstatic 
