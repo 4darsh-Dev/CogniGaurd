96a50f36a1f00b9cd55eac2b7babc4010e1df6d2
ADARSH MAURYA <109789509+4darsh-Dev@users.noreply.github.com>
2024-06-04 13:30:52 +0530

    Update build_files.sh

--- django-web-app/build_files.sh
+++ django-web-app/build_files.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+
 # Install pip and virtualenv if necessary
-python3.12 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 
-# make migrations
-python manage.py migrate
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic 
+python manage.py collectstatic --noinput
+python manage.py migrate
