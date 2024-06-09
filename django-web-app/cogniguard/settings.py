a47ab35396e07ce7c69412c30fcdd5a5204f8ec1
adarsh <adarshmaurya9415@gmail.com>
2024-06-09 16:22:34 +0530

    fixed debug issue

--- django-web-app/cogniguard/settings.py
+++ django-web-app/cogniguard/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('PJ_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1"]
 
