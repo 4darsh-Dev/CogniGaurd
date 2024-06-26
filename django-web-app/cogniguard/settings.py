cafb4708a61f6640693b714b642970d534adabe8
THENIKHILRAJ <nraj99858@gmail.com>
2024-06-26 10:14:18 +0530

    update

--- django-web-app/cogniguard/settings.py
+++ django-web-app/cogniguard/settings.py
@@ -92,7 +92,6 @@ load_dotenv()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
