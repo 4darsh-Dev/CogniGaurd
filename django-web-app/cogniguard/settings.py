baee042c0199f42b5cf930611153bd62e0cecb4b
adarsh <adarshmaurya9415@gmail.com>
2024-07-03 01:22:10 +0530

    updated allowed hosts"

--- django-web-app/cogniguard/settings.py
+++ django-web-app/cogniguard/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('PJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["congniguard.onionreads.com", ".vercel.app", ".now.sh", "127.0.0.1", "*.onrender.com", "cognigaurd.onrender.com"]
+ALLOWED_HOSTS = ["*.onionreads.com", ".vercel.app", ".now.sh", "127.0.0.1", "*.onrender.com", "cognigaurd.onrender.com", "13.127.143.237"]
 
 
 # Application definition
